f52da00767f3f31e8a536c1144451e368214d7ef
Amaan Ahmed <ahm.amaan@gmail.com>
2024-07-11 00:55:58 +0530

    yaml file

--- src/app/demo.service.ts
+++ src/app/demo.service.ts
@@ -7,7 +7,7 @@ import { catchError, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DemoService {
-  private apiUrl = 'http://localhost:5000/api/roles';
+  private apiUrl = 'http://localhost:5001/api/roles';
 
   constructor(private http: HttpClient) { }
 
