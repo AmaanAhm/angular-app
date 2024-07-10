628d0c20616f9b8b8e22137b8963d86214316c79
Amaan Ahmed <ahm.amaan@gmail.com>
2024-07-11 02:07:34 +0530

    url change

--- src/app/demo.service.ts
+++ src/app/demo.service.ts
@@ -7,7 +7,7 @@ import { catchError, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DemoService {
-  private apiUrl = 'http://65.2.98.244/:5001/api/roles';
+  private apiUrl = 'http://65.2.98.244/api/roles';
 
   constructor(private http: HttpClient) { }
 
